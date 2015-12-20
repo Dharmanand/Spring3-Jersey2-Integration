19105e6769b9e3d1e2ea58180cf2a370de43a2b7
Dharmanand <kumar.kd20@gmail.com>
2015-12-20 21:21:15 +0530

    first commit !!

---
+++ src/main/java/com/kdsoft/resources/MessageResource.java
@@ -0,0 +1,20 @@
+package com.kdsoft.resources;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.springframework.stereotype.Component;
+
+@Component
+@Path(value = "/messages")
+public class MessageResource {
+	
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getText() {
+		return "Hello World";
+	}
+
+}
