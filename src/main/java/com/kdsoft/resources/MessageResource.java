833dcc824d6ee2b0d56f00c137f96e375fb15e6a
Dharmanand <kumar.kd20@gmail.com>
2015-12-27 02:30:17 +0530

    Jersey Json !!

--- src/main/java/com/kdsoft/resources/MessageResource.java
+++ src/main/java/com/kdsoft/resources/MessageResource.java
@@ -2,7 +2,10 @@ package com.kdsoft.resources;
 
 import java.util.List;
 
+import javax.ws.rs.CookieParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.MatrixParam;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -16,21 +19,31 @@ import com.kdsoft.model.Survey;
 @Component
 @Path(value = "/messages")
 public class MessageResource {
-	
+
 	@Autowired
 	private SurveyDAO surveyDao;
-	
+
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
 	public String getText() {
 		return "Hello World";
 	}
-	
+
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	@Path("/all")
 	public List<Survey> getAllSurvey() {
 		return surveyDao.getAllSurvey();
+	}
+
+	@GET
+	@Path("paths")
+	public String getParamUsingAnnotations(
+			@MatrixParam("param") String matrixParam,
+			@HeaderParam("authoSessionId") String header,
+			@CookieParam("name") String cookie) {
+		return "@MatrixParam: " + matrixParam + " @HeaderParam: " + header
+				+ " @CookieParam: " + cookie;
 	}
 
 }
